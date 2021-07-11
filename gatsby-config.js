2a48375295ba4ae7be15a94657a28a3f63461af0
Tyler Pfledderer <tyler.pfledderer@gmail.com>
2021-07-11 17:33:15 -0400

    Update Chakra Gatsby plugin config to remove the CSS Reset.

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,11 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-    `@chakra-ui/gatsby-plugin`,
+    {
+      resolve: `@chakra-ui/gatsby-plugin`,
+      options: {
+        resetCSS: false,
+      },
+    },
   ],
 }
