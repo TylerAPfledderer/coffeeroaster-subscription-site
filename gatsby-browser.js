08e4e5b4687a83b7e8bf7b44c28a123ac5a60089
Tyler Pfledderer <tyler.pfledderer@gmail.com>
2021-07-12 23:12:41 -0400

    Import Fontsource options for Fraunces and Barlow fonts.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,4 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
+// Import Fonts
+import "@fontsource/fraunces/900.css"
+import "@fontsource/barlow/700.css"
+import "@fontsource/barlow/400.css"
