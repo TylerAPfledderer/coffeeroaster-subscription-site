a6b69fe6982ba1a660f0108871abf34959204f3c
Tyler Pfledderer <tyler.pfledderer@gmail.com>
2021-07-12 23:08:21 -0400

    Update Chakra Extended Theme: - Include 'Fraunces' font stack to fonts heading - Include 'Barlow' font stack to fonts body - Create layerStyles for opening and closing the header nav in mobile view.

--- src/@chakra-ui/gatsby-plugin/theme.js
+++ src/@chakra-ui/gatsby-plugin/theme.js
@@ -1,3 +1,19 @@
 import { extendTheme } from "@chakra-ui/react"
-const theme = {}
+const theme = {
+  fonts: {
+    heading:
+      "Fraunces, 'Palatino Linotype', 'Palatino LT STD', 'Book Antiqua', Georgia, serif",
+    body: "Barlow, 'Helvetica Neue', Helvetica, Arial, sans-serif",
+  },
+  layerStyles: {
+    navOpened: {
+      maxHeight: "full",
+      opacity: "1",
+    },
+    navClosed: {
+      maxHeight: "0",
+      opacity: "0",
+    },
+  },
+}
 export default extendTheme(theme)
