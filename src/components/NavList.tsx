84a3068dc6a3a85f3c7dc5f1539a35b4def4c2f5
Tyler Pfledderer <tyler.pfledderer@gmail.com>
2021-08-04 23:11:48 -0500

    Update NavList component: - Implement NavLinkProps interface in React.FC with the NavListLink dependent component - Change props type 'ComponentProps' to 'ComponentPropsWithRef' to provided further specificity - Add flexbox styles and specifiy height of 44px to the Link component in NavListLink

--- src/components/NavList.tsx
+++ src/components/NavList.tsx
@@ -1,4 +1,4 @@
-import React, { ComponentProps } from "react"
+import React, { ComponentPropsWithRef } from "react"
 import { Link as GatsbyLink } from "gatsby"
 import { Container, Link, List, ListItem, Stack } from "@chakra-ui/react"
 
@@ -7,17 +7,34 @@ interface NavLinkProps {
   path: string
 }
 
-const NavListLink = ({ name, path }: NavLinkProps) => {
+const NavListLink: React.FC<NavLinkProps> = ({ name, path }) => {
   return (
     <ListItem>
-      <Link as={GatsbyLink} to={path}>
+      <Link
+        as={GatsbyLink}
+        to={path}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height="44px"
+      >
         {name}
       </Link>
     </ListItem>
   )
 }
 
-const NavList = (props: ComponentProps<typeof Container>) => {
+/**
+ *
+ * The NavList is primitive in styling as it is used in multiple locations.
+ * These locations layout out and color the links differently.
+ * Therefore, Chakra props are allowed to be passed into this component
+ *  instead of applying them directly to the internal Chakra components.
+ * Props passed here should be ones that provide inheritance
+ *  down to the links themselves, so no further complication necessary.
+ */
+
+const NavList = (props: ComponentPropsWithRef<typeof Container>) => {
   return (
     <Stack as={List} {...props}>
       <NavListLink name="Home" path="/" />
