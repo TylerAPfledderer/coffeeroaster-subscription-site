29b3a180f383982ffe35cc6820289479013e6e6d
Tyler Pfledderer <tyler.pfledderer@gmail.com>
2021-08-20 16:59:19 -0400

    Update Hero component: - Set Styles at the 768px breakpoint - Format to comply with current ESLint rules.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,35 +1,35 @@
-import React from "react"
-import { Button, Flex, Heading, Text } from "@chakra-ui/react"
-import { Link as GatsbyLink } from "gatsby"
-import { LocationProps } from "../types/interfaces"
-import { HeroDataProps } from "./layout"
+import React from 'react';
+import {Box, Button, Flex, Heading, Text} from '@chakra-ui/react';
+import {Link as GatsbyLink} from 'gatsby';
+import {LocationProps} from '../types/interfaces';
+import {HeroDataProps} from './layout';
 
 interface HeroProps extends LocationProps, HeroDataProps {}
 
 const Hero: React.FC<HeroProps> = ({
   pagePath,
-  heroData: { title, description, imageSet },
-}) => {
-  return (
-    <Flex
-      as="section"
-      direction="column"
-      alignItems={{ base: "center", md: "flex-start" }}
-      bgImage={imageSet}
-      bgPos="center"
-      bgSize="cover"
-      bgRepeat="no-repeat"
-      color="white"
-      textAlign={{ md: "left" }}
-      padding="100px 24px"
-      borderRadius="10px"
-      marginTop="72px"
-    >
-      <Heading as="h1" size="4xl" mb="6" maxWidth="492px">
+  heroData: {title, description, imageSet},
+}) => (
+  <Flex
+    as="section"
+    direction="column"
+    alignItems={{base: 'center', md: 'flex-start'}}
+    bgImage={imageSet}
+    bgPos="right"
+    bgSize="cover"
+    bgRepeat="no-repeat"
+    color="white"
+    textAlign={{md: 'left'}}
+    padding={{base: '100px 24px', md: '100px 56px'}}
+    borderRadius="10px"
+    marginTop="76px"
+  >
+    <Box maxW="398px">
+      <Heading as="h1" size="4xl" mb="6">
         {title}
       </Heading>
       <Text maxWidth="445px">{description}</Text>
-      {pagePath === "/" && (
+      {pagePath === '/' && (
         <Button
           as={GatsbyLink}
           to="/subscribe"
@@ -40,8 +40,8 @@ const Hero: React.FC<HeroProps> = ({
           Create your plan
         </Button>
       )}
-    </Flex>
-  )
-}
+    </Box>
+  </Flex>
+);
 
-export default Hero
+export default Hero;
