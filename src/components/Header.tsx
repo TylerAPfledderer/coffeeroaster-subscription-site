89a6559326fea0215b4afb5c72722cd9c64674ca
Tyler Pfledderer <tyler.pfledderer@gmail.com>
2021-08-20 17:04:36 -0400

    Format Header component to comply with current ESLint Rules.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,16 @@
-import React from "react"
-import { Box } from "@chakra-ui/react"
-import Hero from "./Hero"
-import { LocationProps } from "../types/interfaces"
-import { HeroDataProps } from "./layout"
+import React from 'react';
+import {Box} from '@chakra-ui/react';
+import Hero from './Hero';
+import {LocationProps} from '../types/interfaces';
+import {HeroDataProps} from './layout';
 
 // Define props here to pass typping in parent component
 interface HeaderProps extends LocationProps, HeroDataProps {}
 
-const Header: React.FC<HeaderProps> = ({ pagePath, heroData }) => {
-  return (
-    <Box as="header" maxW="1440px" mx="auto">
-      <Hero pagePath={pagePath} heroData={heroData} />
-    </Box>
-  )
-}
+const Header: React.FC<HeaderProps> = ({pagePath, heroData}) => (
+  <Box as="header" maxW="1440px" mx="auto">
+    <Hero pagePath={pagePath} heroData={heroData} />
+  </Box>
+);
 
-export default Header
+export default Header;
