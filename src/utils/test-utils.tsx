f65dedb1ea33c4946604570c6035dffc072ab4c8
Tyler Pfledderer <tyler.pfledderer@gmail.com>
2021-08-28 11:10:44 -0400

    Refactor test-utils file to create a CustomRender interface for the function of the same name.

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -1,16 +1,15 @@
 import React, {FC, ReactElement} from 'react';
 import {ChakraProvider} from '@chakra-ui/react';
-import {render, RenderOptions} from '@testing-library/react';
+import {render, RenderOptions, RenderResult} from '@testing-library/react';
+import {queries} from '@testing-library/dom';
 
-const AllTheProviders: FC = ({children}) => (
-  <ChakraProvider>{children}</ChakraProvider>
-);
+const AllTheProviders: FC = ({children}) => <ChakraProvider>{children}</ChakraProvider>;
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-const customRender = (
-  ui: ReactElement,
-  options?: Omit<RenderOptions, 'wrapper'>
-) => render(ui, {wrapper: AllTheProviders, ...options});
+interface CustomRender {
+  (ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>): RenderResult<typeof queries, HTMLElement>;
+}
+
+const customRender: CustomRender = (ui, options) => render(ui, {wrapper: AllTheProviders, ...options});
 
 export * from '@testing-library/react';
 export {customRender as render};
