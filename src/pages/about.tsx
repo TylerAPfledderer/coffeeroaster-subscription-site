ab5e44fc92bb1d193eb973232941235a2a42f565
Tyler Pfledderer <tyler.pfledderer@gmail.com>
2021-09-04 15:47:52 -0400

    Update about page: - Reconfigure props passed to the Layout component. - Create query paths for the Hero background image set and the Commitment section set. - Build the Commitment Statement section and set up the remaining section wrappers with TODO comments. - Add image files for hero and Commitmenet Statement sections to query.

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,26 +1,95 @@
-import {PageProps} from 'gatsby';
+import {Box, Heading, Text} from '@chakra-ui/layout';
+import {Image} from '@chakra-ui/react';
+import MainSection from '../components/MainSection';
+import {graphql, PageProps, useStaticQuery} from 'gatsby';
 import React from 'react';
 import Layout from '../components/layout';
 import Seo from '../components/seo';
-import WhitecupMobileImg from '../images/about/hero/whitecup-mobile.jpg';
-import WhitecupTabletImg from '../images/about/hero/whitecup-tablet.jpg';
-import WhitecupDesktopImg from '../images/about/hero/whitecup-desktop.jpg';
 
-const AboutPage: React.FC<PageProps> = ({path}) => {
+const AboutPage: React.FC<PageProps> = () => {
+  const {
+    WhitecupMobileImg: {publicURL: whitecupMobile},
+    WhitecupTabletImg: {publicURL: whitecupTablet},
+    WhitecupDesktopImg: {publicURL: whitecupDesktop},
+    CommitmentHeaderMobileImg: {publicURL: commitmentHeaderMobile},
+    CommitmentHeaderTabletImg: {publicURL: commitmentHeaderTablet},
+    CommitmentHeaderDesktopImg: {publicURL: commitmentHeaderDesktop},
+  } = useStaticQuery(graphql`
+    query AboutDataQuery {
+      WhitecupMobileImg: file(name: {regex: "/whitecup-mobile/"}) {
+        publicURL
+      }
+      WhitecupTabletImg: file(name: {regex: "/whitecup-tablet/"}) {
+        publicURL
+      }
+      WhitecupDesktopImg: file(name: {regex: "/whitecup-desktop/"}) {
+        publicURL
+      }
+      CommitmentHeaderMobileImg: file(name: {regex: "/image-commitment-mobile/"}) {
+        publicURL
+      }
+      CommitmentHeaderTabletImg: file(name: {regex: "/image-commitment-tablet/"}) {
+        publicURL
+      }
+      CommitmentHeaderDesktopImg: file(name: {regex: "/image-commitment-desktop/"}) {
+        publicURL
+      }
+    }
+  `);
+
   const aboutHero = {
     title: 'About Us',
     description:
       'Coffeeroasters began its journey of exotic discovery in 1999, highlighting stories of coffee from around the world. We have since been dedicated to bring the perfect cup - from bean to brew - in every shipment.',
     imageSet: {
-      base: WhitecupMobileImg,
-      sm: WhitecupTabletImg,
-      xl: WhitecupDesktopImg,
+      base: whitecupMobile,
+      md: whitecupTablet,
+      xl: whitecupDesktop,
     },
   };
   return (
-    <Layout location={path} heroData={aboutHero}>
+    <Layout heroData={aboutHero}>
       <Seo title="About Us" />
-      Main section
+      {/* TODO: Finish Commitment Statement section */}
+      <MainSection flexDirection={{base: 'column', md: 'row'}} justifyContent="center">
+        <Image
+          src={commitmentHeaderMobile}
+          srcSet={`${commitmentHeaderDesktop} 445w,
+              ${commitmentHeaderTablet} 281w,
+              ${commitmentHeaderMobile} 654w`}
+          sizes="(min-width: 1440px) 445px,
+            (min-width: 768px) 281px,
+            654px"
+          borderRadius="8px"
+          fit="cover"
+          objectPosition="top center"
+          width="full"
+          maxWidth={{base: '327px', md: '281px', lg: '445px'}}
+          height={{base: '400px', md: '470px', lg: '520px'}}
+          mb={{base: '48px', md: 0}}
+          mr={{md: '40px', xl: '128px'}}
+        />
+        <Box textAlign={{xl: 'left'}}>
+          <Heading mb={{base: '10', md: '6'}}>Our Commitment</Heading>
+          <Text>
+            We’re built on a simple mission and a commitment to doing good along the way. We want to make it
+            easy for you to discover and brew the world’s best coffee at home. It all starts at the source. To
+            locate the specific lots we want to purchase, we travel nearly 60 days a year trying to understand
+            the challenges and opportunities in each of these places. We collaborate with exceptional coffee
+            growers and empower a global community of farmers through with well above fair-trade benchmarks.
+            We also offer training, support farm community initiatives, and invest in coffee plant science.
+            Curating only the finest blends, we roast each lot to highlight tasting profiles distinctive to
+            their native growing region.
+          </Text>
+        </Box>
+      </MainSection>
+      {/* TODO: Quality Statement section */}
+      <MainSection>Quality Statement</MainSection>
+      {/* TODO: Headquarters list section
+       * - Provide the list via JSON query
+       * - Contains svg, name of country, street, city, state, and phone number
+       */}
+      <MainSection>Headquarters</MainSection>
     </Layout>
   );
 };
