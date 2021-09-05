ffe85a76fb98ac3f853d1b3b4800fa54db793131
Tyler Pfledderer <tyler.pfledderer@gmail.com>
2021-09-04 22:25:00 -0400

    Update about page: complete the Commitment Statement section.

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,4 +1,4 @@
-import {Box, Heading, Text} from '@chakra-ui/layout';
+import {Box, Flex, Heading, Text} from '@chakra-ui/layout';
 import {Image} from '@chakra-ui/react';
 import MainSection from '../components/MainSection';
 import {graphql, PageProps, useStaticQuery} from 'gatsby';
@@ -50,38 +50,46 @@ const AboutPage: React.FC<PageProps> = () => {
   return (
     <Layout heroData={aboutHero}>
       <Seo title="About Us" />
-      {/* TODO: Finish Commitment Statement section */}
-      <MainSection flexDirection={{base: 'column', md: 'row'}} justifyContent="center">
-        <Image
-          src={commitmentHeaderMobile}
-          srcSet={`${commitmentHeaderDesktop} 445w,
+      {/* Commitment Statement section */}
+      <MainSection>
+        <Flex
+          paddingX={{md: '56px', xl: '88px'}}
+          flexDirection={{base: 'column', md: 'row'}}
+          justifyContent="space-between"
+          alignItems="inherit"
+          width="full"
+        >
+          <Image
+            src={commitmentHeaderMobile}
+            srcSet={`${commitmentHeaderDesktop} 445w,
               ${commitmentHeaderTablet} 281w,
               ${commitmentHeaderMobile} 654w`}
-          sizes="(min-width: 1440px) 445px,
+            sizes="(min-width: 1440px) 445px,
             (min-width: 768px) 281px,
             654px"
-          borderRadius="8px"
-          fit="cover"
-          objectPosition="top center"
-          width="full"
-          maxWidth={{base: '327px', md: '281px', lg: '445px'}}
-          height={{base: '400px', md: '470px', lg: '520px'}}
-          mb={{base: '48px', md: 0}}
-          mr={{md: '40px', xl: '128px'}}
-        />
-        <Box textAlign={{xl: 'left'}}>
-          <Heading mb={{base: '10', md: '6'}}>Our Commitment</Heading>
-          <Text>
-            We’re built on a simple mission and a commitment to doing good along the way. We want to make it
-            easy for you to discover and brew the world’s best coffee at home. It all starts at the source. To
-            locate the specific lots we want to purchase, we travel nearly 60 days a year trying to understand
-            the challenges and opportunities in each of these places. We collaborate with exceptional coffee
-            growers and empower a global community of farmers through with well above fair-trade benchmarks.
-            We also offer training, support farm community initiatives, and invest in coffee plant science.
-            Curating only the finest blends, we roast each lot to highlight tasting profiles distinctive to
-            their native growing region.
-          </Text>
-        </Box>
+            borderRadius="8px"
+            fit="cover"
+            objectPosition="top center"
+            width="full"
+            maxWidth={{base: '327px', md: '281px', lg: '445px'}}
+            height={{base: '400px', md: '470px', lg: '520px'}}
+            mb={{base: '48px', md: 0}}
+            mr={{md: '40px', xl: '128px'}}
+          />
+          <Box textAlign={{xl: 'left'}}>
+            <Heading mb={{base: '10', md: '6'}}>Our Commitment</Heading>
+            <Text>
+              We’re built on a simple mission and a commitment to doing good along the way. We want to make it
+              easy for you to discover and brew the world’s best coffee at home. It all starts at the source.
+              To locate the specific lots we want to purchase, we travel nearly 60 days a year trying to
+              understand the challenges and opportunities in each of these places. We collaborate with
+              exceptional coffee growers and empower a global community of farmers through with well above
+              fair-trade benchmarks. We also offer training, support farm community initiatives, and invest in
+              coffee plant science. Curating only the finest blends, we roast each lot to highlight tasting
+              profiles distinctive to their native growing region.
+            </Text>
+          </Box>
+        </Flex>
       </MainSection>
       {/* TODO: Quality Statement section */}
       <MainSection>Quality Statement</MainSection>
