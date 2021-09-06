90cc8cd4443eaed56edc88b254a34ffcdc76e412
Tyler Pfledderer <tyler.pfledderer@gmail.com>
2021-09-06 17:02:40 -0400

    Add footer and navbar test files with snapshots.

---
+++ src/components/layout/__tests__/footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {render} from 'test-utils';
+import Footer from '../Footer';
+
+describe('Footer', () => {
+  it('renders correctly', () => {
+    const tree = render(<Footer />);
+    expect(tree).toMatchSnapshot();
+  });
+});
---
+++ src/components/layout/__tests__/navbar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {render} from 'test-utils';
+import NavBar from '../NavBar';
+
+describe('Navbar', () => {
+  it('renders correctly', () => {
+    const tree = render(<NavBar />);
+    expect(tree).toMatchSnapshot();
+  });
+});
