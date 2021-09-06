b639659f324cd171d66579583cbc2df0a2e6500d
Tyler Pfledderer <tyler.pfledderer@gmail.com>
2021-09-06 15:29:04 -0400

    Remove unused as and section props from MainSection component.

--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -4,15 +4,10 @@ import React from 'react';
 
 const MainSection = forwardRef<FlexProps, 'section'>(({children, ...props}, ref) => (
   <chakra.section
-    as="section"
     position="relative"
     marginTop="32"
     display="flex"
     alignItems="center"
-    /**
-     * The prop's default spacing value of 0.5rem not needed. Call a new value on the custom component if a value is to be set
-     */
-    spacing="0"
     flexDirection={{base: 'column'}}
     {...props}
     ref={ref}
