97cd7b909e9b082e88aebf42002172d23d46d597
Tyler Pfledderer <tyler.pfledderer@gmail.com>
2021-10-09 21:54:25 -0400

    fix(NavBar.tsx): Add state and useEffect to check for rehydration of the client, and update checks that produce dynamic visibility changes to the navigation.
    First check to make sure the component is mounted (via use of state and the useEffect hook) and apply to the ternary operation that determines if the menu is toggled open or if the screen is of tablet size or larger to show that navigation.

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -1,6 +1,6 @@
 import { CloseIcon, HamburgerIcon } from '@chakra-ui/icons';
 import { Box, Button, Container, Flex, useDisclosure, useMediaQuery, VisuallyHidden } from '@chakra-ui/react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Logo from './Logo';
 import NavList from './NavList';
 
@@ -12,6 +12,20 @@ const NavBar: React.FC = () => {
   // Toggling Nav Menu (on the small screen)
   const { isOpen: isMenuOpen, onToggle: setMenuToggle } = useDisclosure();
   const [isGreaterThan768] = useMediaQuery('(min-width: 768px)');
+
+  // Check for client to be mounted (hydration)
+  const [mounted, setMounted] = useState(false);
+
+  // Check if client has been re-hydrated
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  // This check for visible nav menu is to prevent a mismatch between the client and server
+  // with useMediaQuery (see docs warning: https://chakra-ui.com/docs/hooks/use-media-query#return-value)
+  //
+  // Check used to provide change in styled visibility with either the toggling of the nav in mobile or ensure visibility when on a tablet or larger screen
+  const isVisibleNav = mounted && (isGreaterThan768 || isMenuOpen);
   return (
     <Box position="fixed" top="0" left="0" zIndex="docked" width="full">
       <Flex justifyContent="space-between" alignItems="center" py="4" bg="white" layerStyle="layoutBase">
@@ -45,7 +59,7 @@ const NavBar: React.FC = () => {
           bgGradient="linear(#fff 50%, transparent)"
           transition="max-height .3s, opacity .5s"
           zIndex="overlay"
-          layerStyle={isGreaterThan768 || isMenuOpen ? 'navOpened' : 'navClosed'}
+          layerStyle={isVisibleNav ? 'navOpened' : 'navClosed'}
         >
           <NavList
             listStyleType="none"
